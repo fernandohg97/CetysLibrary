be42e739d284d20be6c0f9ee793de0eff4d793a3
Fernando Hernandez <fernando.hdz97@gmail.com>
2018-01-02 03:33:19 -0800

    Add location strategy

--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   port: process.env.PORT || 8482,
-  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/reservationsCetys'
+  db: process.env.MONGODB || 'mongodb://localhost:27017/reservationsCetys'
 }
--- app/ngLibrary/src/app/app.module.ts
+++ app/ngLibrary/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { HomeModule } from './home/home.module';
 import Chart from 'chart.js';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   // All components
@@ -18,7 +19,7 @@ import Chart from 'chart.js';
     HomeModule,
     AdminModule
   ],
-  providers: [], // Services are here
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}], // Services are here
   bootstrap: [AppComponent]
 })
 export class AppModule { }
