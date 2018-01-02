3956cb188a53038cbf70a1769a44325c154132cc
Fernando Hernandez <fernando.hdz97@gmail.com>
2018-01-02 03:56:24 -0800

    stuff

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
+app.use(express.static(__dirname +'/dist'))
 
 app.use('/api', userRouter)
 app.use('/api', reservationRouter)
--- app/ngLibrary/src/app/app.routing.module.ts
+++ app/ngLibrary/src/app/app.routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
+
 // import { ReservationCreateComponent } from './home/reservation/reservation-create/reservation-create.component'
 // import { ReservationUpdateComponent } from './home/reservation/reservation-update/reservation-update.component'
 
