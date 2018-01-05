ee02be7829de8a23ca426417475070799dcd7313
Fernando Hernandez <fernando.hdz97@gmail.com>
2018-01-04 21:49:26 -0800

    stuff

--- app/ngLibrary/src/app/home/home-routing/home.routing.module.ts
+++ app/ngLibrary/src/app/home/home-routing/home.routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { HomeComponent } from '../home.component'
 import { ReportsComponent } from '../reports/reports.component';
 import { ReservationCreateComponent } from '../reservation/reservation-create/reservation-create.component';
 import { ReservationUpdateComponent } from '../reservation/reservation-update/reservation-update.component';
@@ -9,12 +8,10 @@ import { AdminComponent } from '../../admin/admin.component';
 
 const homeRoutes: Routes = [
   { path: 'reservations', component: ReportsComponent },
-  // { path: 'home', component: HomeComponent },
   { path: 'reservations-create/:id', component: ReservationCreateComponent },
   { path: 'reservations-update/:id', component: ReservationUpdateComponent },
   { path: 'reservations-edit/:id', component: ReservationEditComponent },
   { path: 'admin', component: AdminComponent }
-
   // { path: '**', component: PageNotFoundComponent } Route in case page is not found
 ]
 
@@ -25,6 +22,4 @@ const homeRoutes: Routes = [
   exports: [RouterModule]
 })
 
-export class HomeRoutingModule {
-
-}
+export class HomeRoutingModule {}
