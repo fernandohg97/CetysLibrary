49170277a225b05b2d1646412004dbee3efb163a
Fernando Hernandez <fernando.hdz97@gmail.com>
2018-01-04 21:48:35 -0800

    Pipe and popup added

--- app/ngLibrary/src/app/home/reports/reports.module.ts
+++ app/ngLibrary/src/app/home/reports/reports.module.ts
@@ -4,12 +4,16 @@ import { ReportsComponent } from './reports.component';
 import { ReportsUserDetailsComponent } from './reports-user-details/reports-user-details.component';
 import {NgxPaginationModule} from 'ngx-pagination'; // <-- import the module
 import { FormsModule } from '@angular/forms';
+import { NguiPopupModule } from '@ngui/popup';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   imports: [
     CommonModule,
     NgxPaginationModule,
-    FormsModule
+    FormsModule,
+    NguiPopupModule,
+    PipesModule.forRoot()
   ],
   declarations: [
     ReportsComponent,
