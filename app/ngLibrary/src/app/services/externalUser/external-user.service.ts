b35d0071c202b2adf64f9ce3c565e917e85687e7
Fernando <fernando.hdz97@gmail.com>
2018-01-13 11:17:26 -0800

    external user service created

--- app/ngLibrary/src/app/services/externalUser/external-user.service.ts
+++ app/ngLibrary/src/app/services/externalUser/external-user.service.ts
@@ -1,8 +1,55 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import { ExternalUserModel } from '../../models/externalUser.model';
+import { environment } from '../../../environments/environment';
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/toPromise";
 
 @Injectable()
 export class ExternalUserService {
 
-  constructor() { }
+  private url = `${environment.baseUrl}/api/externalUsers`
+
+  constructor(private http: Http) { }
+
+  private static handleError(error: any): Promise<any> {
+    console.error("An error occurred", error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  getAll(): Promise<ExternalUserModel[]> {
+    return this.http.get(this.url)
+    .toPromise()
+    .then(res => res.json() as ExternalUserModel[])
+    .catch(ExternalUserService.handleError)
+  }
+
+  getById(_id: String): Promise<ExternalUserModel> {
+    return this.http.get(`${this.url}/${_id}`)
+    .toPromise()
+    .then(res => res.json() as ExternalUserModel)
+    .catch(ExternalUserService.handleError)
+  }
+
+  create(newExternalUser: ExternalUserModel) {
+    return this.http.post(this.url, newExternalUser)
+    .map(res => res.json())
+    .catch(err => ExternalUserService.handleError(err))
+  }
+
+  createFile(newExternalUsers: ExternalUserModel[]) {
+    return this.http.post(this.url, newExternalUsers)
+    .map(res => res.json())
+    .catch(err => ExternalUserService.handleError(err))
+  }
+
+  update(id: String, externalUserModel: ExternalUserModel) {
+    return this.http.put(`${this.url}/${id}`, externalUserModel).toPromise()
+  }
+
+  remove(id: String) {
+    console.log(id)
+    return this.http.delete(`${this.url}/${id}`).toPromise()
+  }
 
 }
