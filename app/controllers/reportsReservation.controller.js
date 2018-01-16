d226f106278cc1a64f1e9f28102ded36745c9a47
Fernando Hernandez <fernando.hdz97@gmail.com>
2018-01-15 23:56:54 -0800

    getByDepartment method created

--- app/controllers/reportsReservation.controller.js
+++ app/controllers/reportsReservation.controller.js
@@ -5,10 +5,29 @@ const Reservation = require('../models/reservation/reservation.model')
 function getReportsByDivision(req, res) {
   let findReservationsDivision = Reservation.aggregate( [
     { $match: { reservationDate: { $gte: new Date(req.query.start), $lte: new Date(req.query.end) } } },
-    { $group: { "_id": "$user.division", "ingresos": { $sum: 1 } } }
+    { $group: { _id: "$user.division", "ingresos": { $sum: 1 } } }
    ])
 
   findReservationsDivision.then(data => {
+    if (data) {
+      console.log(data);
+      res.json(data)
+    } else {
+      res.status(404).send({message: `Page not found`})
+    }
+  })
+  .catch(err => {
+    res.status(500).send({message: `Error del server: ${err}`})
+  })
+}
+
+function getReportsByDepartment(req, res) {
+  let findReservationsDepartment = Reservation.aggregate( [
+    { $match: { reservationDate: { $gte: new Date(req.query.start), $lte: new Date(req.query.end) } } },
+    { $group: { _id: "$employee.department", "ingresos": { $sum: 1 } } }
+   ])
+
+  findReservationsDepartment.then(data => {
     if (data) {
       res.json(data)
     } else {
@@ -62,13 +81,10 @@ function getReportsByDay(req, res) {
   })
 }
 
-// function getReportsByDepartment(req, res) {
-//   let findReservationsByDepartment = Reservation.aggregate([{ $group: { _id: "$"}}])
-// }
-
 module.exports = {
   getReportsByCareer,
   getReportsByCubicle,
   getReportsByDivision,
-  getReportsByDay
+  getReportsByDay,
+  getReportsByDepartment
 }
