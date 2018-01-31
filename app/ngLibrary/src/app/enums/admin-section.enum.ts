6de8eccd623a3906192de0ebdc7b045eafe19377
Fernando Hernandez <fernando.hdz97@gmail.com>
2018-01-31 08:40:52 -0800

    Add admin section enum

---
+++ app/ngLibrary/src/app/enums/admin-section.enum.ts
@@ -0,0 +1,15 @@
+// export enum SchoolDivision {
+//   Preparatoria = 1,
+//   Ingenieria = 2,
+//   Posgrado = 3,
+//   Doctorado = 4,
+//   Externo = 5
+// }
+export enum AdminSection {
+  students = 0,
+  cubicles = 1,
+  employees = 2,
+  careers = 3,
+  departments = 4,
+  reservations = 5
+}
