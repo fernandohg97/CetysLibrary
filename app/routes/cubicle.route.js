dfac369395503b37a0c0bd119f6e2f5b258ff594
Fernando Hernandez <fernando.hdz97@gmail.com>
2018-03-14 15:11:42 -0700

    document routes file

--- app/routes/cubicle.route.js
+++ app/routes/cubicle.route.js
@@ -1,28 +1,28 @@
 'use strict'
 
-const express = require('express')
-const cubicleRouter = express.Router()
-const cubicleCtrl = require('../controllers/cubicle.controller')
-const created = require('../middlewares/cubicleCreated')
+const express = require('express') // import express library
+const cubicleRouter = express.Router() // declare router with express
+const cubicleCtrl = require('../controllers/cubicle.controller') // import cubicle controller
+const created = require('../middlewares/cubicleCreated') // import cubicle middleware
+// Endpoints
+cubicleRouter.get('/cubicles', cubicleCtrl.getCubicles) // Get all cubicles
 
-cubicleRouter.get('/cubicles', cubicleCtrl.getCubicles)
+cubicleRouter.get('/cubicles/count', cubicleCtrl.getCubiclesCount) // Get the maximun number of cubicles
 
-cubicleRouter.get('/cubicles/count', cubicleCtrl.getCubiclesCount)
+cubicleRouter.get('/cubicles/file', cubicleCtrl.createCubiclesFile) // Create cubicles local file
 
-cubicleRouter.get('/cubicles/file', cubicleCtrl.createCubiclesFile)
+cubicleRouter.get('/cubicles/download', cubicleCtrl.downloadCubiclesFile) // Download cubicles local file
 
-cubicleRouter.get('/cubicles/download', cubicleCtrl.downloadCubiclesFile)
+cubicleRouter.get('/cubicles/remove', cubicleCtrl.removeCubiclesFile) // Remove cubicles local file
 
-cubicleRouter.get('/cubicles/remove', cubicleCtrl.removeCubiclesFile)
+cubicleRouter.get('/cubicles/:cubicle_id', cubicleCtrl.getCubicle) // Get cubicle by id
 
-cubicleRouter.get('/cubicles/:cubicle_id', cubicleCtrl.getCubicle)
+cubicleRouter.post('/cubicles', created.isCreated, cubicleCtrl.createCubicle) // Create cubicle
 
-cubicleRouter.post('/cubicles', created.isCreated, cubicleCtrl.createCubicle)
+cubicleRouter.put('/cubicles/:cubicle_id', cubicleCtrl.updateCubicle) // Update cubicle
 
-cubicleRouter.put('/cubicles/:cubicle_id', cubicleCtrl.updateCubicle)
+cubicleRouter.delete('/cubicles/:cubicle_id', cubicleCtrl.removeCubicle) // Delete cubicle by id
 
-cubicleRouter.delete('/cubicles/:cubicle_id', cubicleCtrl.removeCubicle)
-
-cubicleRouter.delete('/cubicles', cubicleCtrl.removeCubicles)
+cubicleRouter.delete('/cubicles', cubicleCtrl.removeCubicles) // Delete all cubicles
 
 module.exports = cubicleRouter
--- app/routes/reports.route.js
+++ app/routes/reports.route.js
@@ -1,24 +1,23 @@
 'use strict'
 
-const express = require('express')
-const reportsRouter = express.Router()
-const app = express()
-const reportsCtrl = require('../controllers/reportsReservation.controller')
+const express = require('express') // import express library
+const reportsRouter = express.Router() // declare router with express
+const reportsCtrl = require('../controllers/reportsReservation.controller') // import reports controller
+// Endpoints
+reportsRouter.get('/reportsDivision', reportsCtrl.getReportsByDivision) // Get reports by division
 
-reportsRouter.get('/reportsDivision', reportsCtrl.getReportsByDivision)
+reportsRouter.get('/reportsCareer', reportsCtrl.getReportsByCareer) // Get reports by career
 
-reportsRouter.get('/reportsCareer', reportsCtrl.getReportsByCareer)
+reportsRouter.get('/reportsCubicle', reportsCtrl.getReportsByCubicle) // Get reports by cubicle
 
-reportsRouter.get('/reportsCubicle', reportsCtrl.getReportsByCubicle)
+reportsRouter.get('/reportsDay', reportsCtrl.getReportsByDay) // Get reports by day
 
-reportsRouter.get('/reportsDay', reportsCtrl.getReportsByDay)
+reportsRouter.get('/reportsDepartment', reportsCtrl.getReportsByDepartment) // Get reports by department
 
-reportsRouter.get('/reportsDepartment', reportsCtrl.getReportsByDepartment)
+reportsRouter.get('/reportsExternal', reportsCtrl.getReportsByExternalUser) // Get reports by external users
 
-reportsRouter.get('/reportsExternal', reportsCtrl.getReportsByExternalUser)
+reportsRouter.get('/reportsCompanionsCareer', reportsCtrl.getReportsByCareerCompanions) // Get reports by career name companions
 
-reportsRouter.get('/reportsCompanionsCareer', reportsCtrl.getReportsByCareerCompanions)
-
-reportsRouter.get('/reportsCompanionsQuantity', reportsCtrl.getReportsByQuantityCompanions)
+reportsRouter.get('/reportsCompanionsQuantity', reportsCtrl.getReportsByQuantityCompanions) // Get reports by career quantity companions
 
 module.exports = reportsRouter
