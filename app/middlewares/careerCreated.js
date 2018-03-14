af0c42dc0c578a69193a2d9f89ff0b1ba85b4a68
Fernando Hernandez <fernando.hdz97@gmail.com>
2018-03-14 15:11:15 -0700

    document middlewares file

--- app/middlewares/careerCreated.js
+++ app/middlewares/careerCreated.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const Career = require('../models/career/career.model')
-
+const Career = require('../models/career/career.model') // import career model
+// Get careers and validate if a career exist
 exports.isCreated = function (req, res, next) {
-  let dbCareers = []
-  let careersUser = req.body
+  let dbCareers = [] // Careers in database
+  let careersUser = req.body // Careers from user
   Career.find((err, careers) => {
     if (err) return res.status(500).send({message: `Error del server ${err}`})
     if (careers) {
       dbCareers = careers
       let value = checkCareers(dbCareers, careersUser)
-      if (value) return res.status(403).send({
+      if (value) return res.status(403).send({ // In case a career exist
         existCareer: 'Esta carrera ya existe',
         existCareers: 'Hay carreras ya existentes en el archivo'
       })
@@ -18,16 +18,16 @@ exports.isCreated = function (req, res, next) {
     }
   })
 }
-
+// Check if career or careers are alredy in database
 function checkCareers (careers, careersUser) {
   let exist = false
-  if (careersUser.length == undefined) {
+  if (careersUser.length == undefined) { // In case is only one career
     careers.forEach(career => {
       if (career.careerCode === careersUser.careerCode) {
         exist = true
       }
     })
-  } else {
+  } else { // In case there are many careers that want to be created
     careers.forEach(career => {
       for (var i = 0; i < careersUser.length; i++) {
         if (career.careerCode === careersUser[i].careerCode) {
--- app/middlewares/cubicleCreated.js
+++ app/middlewares/cubicleCreated.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const Cubicle = require('../models/cubicle/cubicle.model')
-
+const Cubicle = require('../models/cubicle/cubicle.model') // import cubicle model
+// Get cubicles and validate if a cubicle exist
 exports.isCreated = function (req, res, next) {
-  let dbCubicles = []
-  let cubiclesUser = req.body
+  let dbCubicles = [] // Cubicles in database
+  let cubiclesUser = req.body // Cubicles from the user
   Cubicle.find((err, cubicles) => {
     if (err) return res.status(500).send({message: `Error del server ${err}`})
     if (cubicles) {
       dbCubicles = cubicles
       let value = checkCubicles(dbCubicles, cubiclesUser)
-      if (value) return res.status(403).send({
+      if (value) return res.status(403).send({ // In case a cubicle existe
         existCubicle: 'Este cubiculo ya existe',
         existCubicles: 'Hay cubiculos ya existentes en el archivo'
       })
@@ -18,16 +18,16 @@ exports.isCreated = function (req, res, next) {
     }
   })
 }
-
+// Check if cubicle or cubicles are alredy in database
 function checkCubicles (cubicles, cubiclesUser) {
   let exist = false
-  if (cubiclesUser.length == undefined) {
+  if (cubiclesUser.length == undefined) { // In case is only one cubicle
     cubicles.forEach(cubicle => {
       if (cubicle.cubicleNumber === cubiclesUser.cubicleNumber) {
         exist = true
       }
     })
-  } else {
+  } else { // In case there are many cubicles that want to be created
     cubicles.forEach(cubicle => {
       for (var i = 0; i < cubiclesUser.length; i++) {
         if (cubicle.cubicleNumber === cubiclesUser[i].cubicleNumber) {
