3cb55e6f6c161cbd706d89dcda943e0cc3a235e4
Fernando Hernandez <fernando.hdz97@gmail.com>
2018-03-18 20:13:35 -0700

    Document enums

--- app/ngLibrary/src/app/enums/admin-section.enum.ts
+++ app/ngLibrary/src/app/enums/admin-section.enum.ts
@@ -1,3 +1,6 @@
+// To know which admin element you want to delete all documents
+// This is called when you want to remove all files
+// For example if you press the button to remove all the students.
 export enum AdminSection {
   students = 0,
   cubicles = 1,
