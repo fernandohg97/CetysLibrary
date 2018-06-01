978645a9dce92d846d8b47acbd60eb04dffa8606
Fernando Hernandez <fernando.hdz97@gmail.com>
2018-06-01 03:05:24 -0700

    Change id attribute to _id

--- app/ngLibrary/src/app/models/companion.model.ts
+++ app/ngLibrary/src/app/models/companion.model.ts
@@ -1,15 +1,17 @@
 export class CompanionModel {
-  id: String
+  _id: String
   reservationDate: Date;
   quantity: number;
+  reservation: String;
   registrationNumber: number;
   userCode: String;
   division: String;
   career: String;
   department: String;
-  constructor (reservationDate: Date, quantity: number, registrationNumber?: number, userCode?: String, division?: String, career?: String, department?: String) {
+  constructor (reservationDate: Date, quantity: number, reservation: String, registrationNumber?: number, userCode?: String, division?: String, career?: String, department?: String) {
     this.reservationDate = reservationDate;
     this.quantity = quantity;
+    this.reservation = reservation;
     this.registrationNumber = registrationNumber;
     this.userCode = userCode;
     this.division = division;
